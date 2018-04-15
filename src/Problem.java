406cdb6f34694d357f9d6bafdf0311a2b638bd11
Matek1731 <matek.sl@wp.pl>
2018-04-15 16:52:41 +0200

    tried to fix stuff and refactor the code, hope it works

--- src/Problem.java
+++ src/Problem.java
@@ -49,6 +49,7 @@ public abstract class Problem {
                 + matrixString;
     }
 
+    //this method does all the nessessary stuff when a row is visited
     public void markAsVisited(int i, int j, boolean[] visited, AdjacencyMatrix mat){
         visited[i] = true;
         visited[j] = true;
@@ -57,92 +58,79 @@ public abstract class Problem {
         }
     }
 
+    //here we use our final method to solve the problem
     public void solveProblem(){
-        oldGreedyAlgorithm();
-
-        //TSP();
+        //oldGreedyAlgorithm();
+        //greedyAlgorithm();
+        TSP();
     }
 
+    //method calculates distance in the modified matrix
     public int calculateDistance(int[][] matrix, int n, List<Integer> tab){
         int distance = matrix[tab.get(tab.size() - 1)][tab.get(0)];
-        //System.out.println("halo dzik");
         for (int i = 0; i < tab.size()-1; i++){
             distance += matrix[tab.get(i)][tab.get(i + 1)];
         }
         return distance;
     }
 
+    //Traveling Salesman Problem
     public void TSP() {
-        //tworzymy nowa macierz ktorej wartosci wynosza 10 - [i][j] macierzy oryginalnej, aby moc minimalizowac
+        //tworzymy nowa macierz ktorej wartosci wynosza
+        // nucleotideLength(w naszym przypadku 10) minus wartosc pola [i][j] macierzy oryginalnej,
+        // aby moc minimalizowac zamiast maksymalizowac (bardziej intuicyjne wg mnie)
         int[][] newMat = new int[n][n];
+        //String newMatrixString sluzy do debugowania mozna podejrzec sobie co jest w nowej macierzy,
+        // a wszystkie linie ktore go dotycza mozna sobie zakomentowac lub odkomentowac
         String newMatrixString = "";
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 newMat[i][j] = nucleotideLength - adjacencyMatrix.getMatrix()[i][j];
-                newMatrixString += newMat[i][j] + " ";
+                //newMatrixString += newMat[i][j] + " ";
             }
-            newMatrixString += '\n';
+            //newMatrixString += '\n';
         }
-        System.out.println(newMatrixString);
-        Random generator = new Random();
+        //System.out.println(newMatrixString);
+
+
         //wczytujemy rozwiazanie zachlanne jako start metody
         List<Integer> indexesOfNodes = greedyAlgorithm();
 
 
         /*
-        //random route
-        Random generator = new Random();
-        List<Integer> indexesOfNodes = new ArrayList<>();
-        newMatrixString = "";
-        while(indexesOfNodes.size() < n) {
-            int next = generator.nextInt(n);
-
-            if(!indexesOfNodes.contains(next)){
-                indexesOfNodes.add(next);
-                System.out.println(next);
-            }
-            else{
-                System.out.println("było: " + next);
-            }
-
-        }
-        */
-        newMatrixString = "";
+        //mozna tu podejrzec co zostalo wczytane przez algorytm zachlanny
+        String TabString = "";
         for (int i = 0; i < indexesOfNodes.size(); i++){
-            newMatrixString+= indexesOfNodes.get(i) + " ";
+            TabString+= indexesOfNodes.get(i) + " ";
         }
+        System.out.println("Wczytalem: "+ TabString);
+        */
 
-        System.out.println("Wczytalem: "+ newMatrixString);
+
+        //obliczamy obecny dystans i przechowujemy go jako bestDistance
         int bestDistance = calculateDistance(newMat, n, indexesOfNodes);
+        //System.out.println("bestDistance: " + bestDistance);
 
-        System.out.println("Dystans: " + bestDistance);
-        System.out.println("Tera jedziemy z zamianami kurde");
-        for (int i = 0; i < 1000000; i++){
+
+        //System.out.println("Tera jedziemy z zamianami kurde");
+        Random generator = new Random();
+        for (int i = 0; i < 1000; i++){  //liczbe iteracji petli mozna zmieniac
+            //losujemy 2 pozycje w tablicy
             int first = generator.nextInt(n);
-            int afterFirst;
-            if (first == n-1){
-                afterFirst = 0;
-            }
-            else{
-                afterFirst = first + 1;
-            }
             int second = generator.nextInt(n);
-            int afterSecond;
-            if (second == n-1){
-                afterSecond = 0;
-            }
-            else{
-                afterSecond = first + 1;
-            }
+            //zamieniamy elementy miejscami
             Integer tmp1 = indexesOfNodes.get(first);
             Integer tmp2 = indexesOfNodes.get(second);
             indexesOfNodes.set(first, tmp2);
             indexesOfNodes.set(second, tmp1);
+            //liczymy dystans po zamianie
             int currentDistance = calculateDistance(newMat, n, indexesOfNodes);
+            //jezeli jest krocej (moze byc < lub <=) zostawiamy i aktualizujemy bestDistance
             if (currentDistance <= bestDistance){
                 bestDistance = currentDistance;
-                System.out.println("lepiej" + i);
+                System.out.println("lepiej " + i);
             }
+            //w przeciwnym przypadku zamieniamy z powrotem
             else {
                 indexesOfNodes.set(first, tmp1);
                 indexesOfNodes.set(second, tmp2);
@@ -150,26 +138,33 @@ public abstract class Problem {
             }
 
         }
+
+
+        /*
+        //wartosci zmiennej bestDistance oraz obecnego dystansu powinny byc sobie rowne
+        // mozna tez patrzec na ktoras z nich zeby podejrzec jak sie zmienila po przemieleniu calej petli
         System.out.println("best: " + bestDistance);
         System.out.println("prawdziwy: " + calculateDistance(newMat, n, indexesOfNodes));
+        */
+
+
         //tera kurde blaszka jedziemy od poczatku i pokazujemy rozwiazanie
-        int lengthOfSequence = 0;
-        int it = 0;
-        for (int i = 0; i < indexesOfNodes.size()-1; i++) {
-            //lengthOfSequence += nucleotideLength - adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext];
-            if(lengthOfSequence +(nucleotideLength - adjacencyMatrix.getMatrix()[indexesOfNodes.get(i)][indexesOfNodes.get(i + 1)]) <= maxLengthOfSequence - 9){
+        int lengthOfSequence = nucleotideLength;
+        //zaczynamy od dlugosci pelnego nukleotydu (10), a liczenie dodatkowych rzeczy zaczynamy od drugiego elementu tablicy
+        for (int i = 1; i < indexesOfNodes.size()-1; i++) {
+            if(lengthOfSequence +(nucleotideLength - adjacencyMatrix.getMatrix()[indexesOfNodes.get(i)][indexesOfNodes.get(i + 1)]) <= maxLengthOfSequence){
                 lengthOfSequence+= (nucleotideLength - adjacencyMatrix.getMatrix()[indexesOfNodes.get(i)][indexesOfNodes.get(i + 1)]);
-                System.out.println(lengthOfSequence);
-                System.out.println(i+1 + ".asd " + indexesOfNodes.get(i) + ": "
+                System.out.println(i + ". " + indexesOfNodes.get(i) + ": "
                         + adjacencyMatrix.getMatrix()[indexesOfNodes.get(i)][indexesOfNodes.get(i+1)]);
             }
             else{
                 break;
             }
         }
-
-
     }
+
+
+
     public boolean isNotVisited(boolean[] tab){
         for (boolean v: tab) {
             if(v == false){
@@ -179,27 +174,20 @@ public abstract class Problem {
         return true;
     }
 
-
+    //returns a list of all nodes ordered by greedy algorithm even if they don't fit into maxNuclotideLength
     public List<Integer> greedyAlgorithm(){
-        //for(int i = 0; i < n; i++) {
-        //    System.out.println(adjacencyMatrix.getRowSum(i));
-        //}
         List<Integer> indexesOfNodes = new ArrayList<>();
         boolean[] visited = new boolean[n];
-        System.out.println(n + "kurdeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-        int[] rowSum = new int[n];
         // find max elem
         int maxIndexI = 0;
         int maxIndexJ = 0;
-        for (int i = 0; i < this.n; i++){
-            rowSum[i] = 0;
-        }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(adjacencyMatrix.getMatrix()[i][j] > adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ]){
                     maxIndexI = i;
                     maxIndexJ = j;
                 }
+                //with a minor improvement using rowSums
                 else if(adjacencyMatrix.getMatrix()[i][j] == adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ]){
                     if(adjacencyMatrix.getRowSum(i) < adjacencyMatrix.getRowSum(maxIndexI)){
                         maxIndexI = i;
@@ -212,45 +200,37 @@ public abstract class Problem {
         indexesOfNodes.add(maxIndexI);
         indexesOfNodes.add(maxIndexJ);
         markAsVisited(maxIndexI, maxIndexJ, visited, adjacencyMatrix);
-        //markAsVisited(maxIndexJ, visited);
-        //visited[maxIndexI] = true;
-        //visited[maxIndexJ] = true;
-        int lengthOfSequence = nucleotideLength + adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ];
+        //wg mnie to jest zle:
+        // int lengthOfSequence = nucleotideLength + adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ];
+        //zrobilnym tak:
+        int lengthOfSequence = nucleotideLength + (nucleotideLength - adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ]);
         int currentNodeIndex = maxIndexJ;
         //while(lengthOfSequence < maxLengthOfSequence)
+        // to bylo w rozwiazaiu zwyklym ale tu interesuja nas nawet te rzeczy ktore sie nie mieszcza zeby potem je przekazac do TSP
+        // zamiast tego lecimy dopoki wszystkie nie zostana odwiedzone
         while (!isNotVisited(visited))
         {
             // find max from current Node
             int maxIndexNext = 0;
             for(int j = 0; j < n; j++){
-                // must be >= because 0 index might be already visited
-                //szefie czemu >= ???
-                //if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] >= adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
-                //        && !visited[j]){
                 if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] > adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
                         && !visited[j]){
                     maxIndexNext = j;
                 }
-                else if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] > adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
+                else if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] == adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
                         && !visited[j]){
                     if(adjacencyMatrix.getRowSum(j) < adjacencyMatrix.getRowSum(maxIndexNext)){
                         maxIndexNext = j;
                     }
                 }
             }
-            //if(lengthOfSequence + nucleotideLength - adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext] <= maxLengthOfSequence)
-            {
-                //visited[maxIndexNext] = true;
-                markAsVisited(currentNodeIndex, maxIndexNext, visited, adjacencyMatrix);
-                indexesOfNodes.add(maxIndexNext);
-                lengthOfSequence += nucleotideLength - adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext];
-                currentNodeIndex = maxIndexNext;
-            }
-            //else{
-            //   break;
-            //}
+            markAsVisited(currentNodeIndex, maxIndexNext, visited, adjacencyMatrix);
+            indexesOfNodes.add(maxIndexNext);
+            currentNodeIndex = maxIndexNext;
         }
 
+
+        //rzeczy do wypisywania, tutaj nie sa potrzebne ale nie bede ich psul
         System.out.println("Number of nucleotides in a seq: " + indexesOfNodes.size()
                 + "/" + maxLengthOfSequence);
         System.out.println("Nodes and weights: ");
@@ -263,22 +243,16 @@ public abstract class Problem {
             ind = ind + indexesOfNodes.get(i) + " ";
         }
         System.out.println("Greedy: " + ind);
+
         return indexesOfNodes;
     }
 
     public void oldGreedyAlgorithm(){
-        //for(int i = 0; i < n; i++) {
-        //    System.out.println(adjacencyMatrix.getRowSum(i));
-        //}
         List<Integer> indexesOfNodes = new ArrayList<>();
         boolean[] visited = new boolean[n];
-        int[] rowSum = new int[n];
         // find max elem
         int maxIndexI = 0;
         int maxIndexJ = 0;
-        for (int i = 0; i < this.n; i++){
-            rowSum[i] = 0;
-        }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(adjacencyMatrix.getMatrix()[i][j] > adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ]){
@@ -297,40 +271,26 @@ public abstract class Problem {
         indexesOfNodes.add(maxIndexI);
         indexesOfNodes.add(maxIndexJ);
         markAsVisited(maxIndexI, maxIndexJ, visited, adjacencyMatrix);
-        //markAsVisited(maxIndexJ, visited);
-        //visited[maxIndexI] = true;
-        //visited[maxIndexJ] = true;
+        //wg mnie to jest zle:
         int lengthOfSequence = nucleotideLength + adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ];
+        // zrobilnym tak:
+        // int lengthOfSequence = nucleotideLength + (nucleotideLength - adjacencyMatrix.getMatrix()[maxIndexI][maxIndexJ]);
         int currentNodeIndex = maxIndexJ;
-        int pom = 1;
         while(lengthOfSequence < maxLengthOfSequence){
             // find max from current Node
             int maxIndexNext = 0;
             for(int j = 0; j < n; j++){
                 // must be >= because 0 index might be already visited
-                //szefie czemu >= ???
-                //if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] >= adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
-                //        && !visited[j]){
-                if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] > adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
+                if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] >= adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
                         && !visited[j]){
                     maxIndexNext = j;
-                }
-                else if(adjacencyMatrix.getMatrix()[currentNodeIndex][j] > adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]
-                        && !visited[j]){
-                    if(adjacencyMatrix.getRowSum(j) < adjacencyMatrix.getRowSum(maxIndexNext)){
-                        maxIndexNext = j;
-                    }
                 }
             }
 
             if(lengthOfSequence + nucleotideLength - adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext] <= maxLengthOfSequence) {
-                //visited[maxIndexNext] = true;
                 markAsVisited(currentNodeIndex, maxIndexNext, visited, adjacencyMatrix);
                 indexesOfNodes.add(maxIndexNext);
                 lengthOfSequence += nucleotideLength - adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext];
-                System.out.println(pom + " " + lengthOfSequence + " " + adjacencyMatrix.getMatrix()[currentNodeIndex][maxIndexNext]);
-                pom++;
-                //kurde blaszka Oli ktoś z nas źle coś liczy kurde bela bo Ty masz 143 a ja 155
                 currentNodeIndex = maxIndexNext;
             }
             else{
@@ -346,9 +306,5 @@ public abstract class Problem {
             System.out.println(i+1 + ". " + indexesOfNodes.get(i) + ": "
                     + adjacencyMatrix.getMatrix()[indexesOfNodes.get(i)][indexesOfNodes.get(i+1)]);
         }
-        //for(int i = 0; i < n; i++) {
-        //    System.out.println(adjacencyMatrix.getRowSum(i));
-        //}
-        //return indexesOfNodes;
     }
 }
