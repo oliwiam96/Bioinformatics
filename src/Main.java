f18e0bb2070f5531f719b49c16ade16c1fa33b91
Oliwia <oliwia.m.96@wp.pl>
2018-05-31 14:17:30 +0200

    Remove classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Problem negProblem = new NegProblem("neg2.txt");
+        Problem negProblem = new Problem("neg2.txt");
         System.out.println(negProblem);
         negProblem.solveProblem();
     }
--- src/Problem.java
+++ src/Problem.java
@@ -1,7 +1,10 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public abstract class Problem {
+public class Problem {
     protected int maxLengthOfSequence;
     protected int nucleotideLength;
     protected int errorsNumber; // neg/ pos errors
@@ -13,7 +16,29 @@ public abstract class Problem {
         readInputFromFile(fileName);
     }
 
-    abstract void readInputFromFile(String fileName);
+    void readInputFromFile(String fileName) {
+        try {
+            Scanner in = new Scanner(new FileReader("examples/" + fileName));
+            this.maxLengthOfSequence = in.nextInt();
+            this.nucleotideLength = in.nextInt();
+            this.errorsNumber = in.nextInt();
+            this.n = in.nextInt();
+
+            //System.out.println("max: "+ maxLengthOfSequence + " l: " + nucleotideLength + " err: " + errorsNumber + " n: " +n);
+            in.nextLine();
+            for (int i = 0; i < n; i++) {
+                String line = in.nextLine();
+                //System.out.println(line);
+                Node node = new Node(line);
+                nodeList.add(node);
+            }
+
+            this.createAdjacencyMatrix();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     /**
      * Warning! nodeList and n must be valid
